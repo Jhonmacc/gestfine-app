d4a0ac4edcf1b9f6e4bc23047d01a96237a45ec3
Jhon Amorim <jhon.macc92@gmail.com>
2024-03-09 07:15:58 -0300

    [Menu add Controle de Certificados]

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('certification.index') }}" :active="request()->routeIs('certification.index')">
+                        {{ __('Controle de Certificados') }}
+                    </x-nav-link>
                 </div>
             </div>
 
