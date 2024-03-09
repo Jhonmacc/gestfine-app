bec85884b282df76350942e5e57a4f96cc7fb1ff
Jhon Amorim <jhon.macc92@gmail.com>
2024-03-09 07:26:05 -0300

    [Add Table Certification]

---
+++ database/migrations/2024_03_09_102400_create_certifications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('certifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('validTo_time_t');
+            $table->string('cnpj_cpf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('certifications');
+    }
+};
