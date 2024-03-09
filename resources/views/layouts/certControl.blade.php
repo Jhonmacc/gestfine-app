41c44ac01bf752f5bee8f054d4ecd984de53b393
Jhon Amorim <jhon.macc92@gmail.com>
2024-03-09 07:36:25 -0300

    [Add Bootstrapp]

--- resources/views/layouts/certControl.blade.php
+++ resources/views/layouts/certControl.blade.php
@@ -4,7 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle de Certificados</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Controle de Certificados</title>
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.6.2/dist/alpine.min.js" defer></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js" defer></script>
+        <script src="https://cdn.jsdelivr.net/npm/livewire/livewire.min.js" defer></script>
 </head>
 <body class="font-sans antialiased">
     <x-banner />
@@ -27,6 +37,7 @@
         </main>
     </div>
     <!-- Livewire Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" defer></script>
     @livewireScripts
 </body>
 </html>
