cbc1eeac31a5218d7ec3032b63644631ea4aba34
Jhon Amorim <jhon.macc92@gmail.com>
2024-03-09 12:22:54 -0300

    [Gráfico Label]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -8,7 +9,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <legend>Gráficos de Certificados aqui</legend>
+                <legend class="badge text-bg-primary span12" style="font-size: 18px;" for="">Gráficos dos Certificados</legend>
+
                 <div class="container">
                     <canvas id="certificateChart" style="max-width: 400px; max-height: 400px;"></canvas>
                 </div>
