28ae9e5e410fccc72aa0ad5ffac3e9a7961c568d
Jhon Amorim <jhon.macc92@gmail.com>
2024-07-06 03:48:48 -0300

    [Sol 27] Alerações de cores do gráfico e nomeclatura card no prazo.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -71,11 +71,11 @@
                                 data: [data.cpfCount, data.cnpjCount],
                                 backgroundColor: [
                                     'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(50, 0, 150, 0.2)',
                                 ],
                                 borderColor: [
                                     'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(50, 0, 150, 1)',
                                 ],
                                 borderWidth: 1
                             }]
