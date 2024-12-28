d863d2afd8c40617c28455a87b7c7c8478d5fbd5
Jhon Amorim <jhon.macc92@gmail.com>
2024-12-28 23:43:54 +0000

    Schedule - > [Alteração do horário de agendamento]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new MonitorCertificadosJob)->dailyAt('8:10');
+        $schedule->job(new MonitorCertificadosJob)->dailyAt('00:00');
     }
     protected function commands(): void
     {
